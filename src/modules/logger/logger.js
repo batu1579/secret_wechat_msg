0b0631d4132568c320689c703a7c63f9a3c6a53c
BATU1579 <batu1234567890@outlook.com>
2022-02-05 15:12:08 +0800

    🌈 style: 修改了拼写错误的方法名

--- src/modules/logger/logger.js
+++ src/modules/logger/logger.js
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-02-05 04:00:16
  * @LastEditor: BATU1579
- * @LastTime: 2022-02-05 04:14:01
+ * @LastTime: 2022-02-05 05:46:03
  * @FilePath: \\src\\modules\\logger\\logger.js
  * @Description: 简单包装一下 console
  */
@@ -12,31 +12,31 @@ export class Logger {
         this.modules_name = modules_name;
     }
 
-    patten (level, message) {
+    pattern (level, message) {
         return "[" + level + "] <" + this.modules_name + "> : " + message;
     }
 
     verbose (message) {
         console.verbose(
-            this.patten("VERBOSE", message)
+            this.pattern("VERBOSE", message)
         );
     }
 
     info (message) {
         console.info(
-            this.patten("INFO", message)
+            this.pattern("INFO", message)
         );
     }
 
     warn (message) {
         console.warn(
-            this.patten("WARN", message)
+            this.pattern("WARN", message)
         );
     }
 
     error (message) {
         console.error(
-            this.patten("ERROR", message)
+            this.pattern("ERROR", message)
         );
     }
 }
